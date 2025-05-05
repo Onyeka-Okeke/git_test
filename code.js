5ee9ed509974d490f7f7da29400b15bba5ad3bec
Onyeka-Okeke <135011195+Onyeka-Okeke@users.noreply.github.com>
2025-05-05 14:54:59 +0100

    Update code.js
    Creating a library (Odin project)

--- code.js
+++ code.js
@@ -1,1 +1,11 @@
-console.log("hello world");
+function Book(title, author, pages, read){
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
+  this.info = function() {
+    return `${this.title} by ${this.author}, ${pages} pages, ${this.read}`;
+  };
+}
+let firstBook = new Book('The Hobbit', 'J.R.R Tolkien', 295, 'read');
+ console.log(firstBook.info());
