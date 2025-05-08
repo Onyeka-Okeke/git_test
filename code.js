6f3c791576fad7b7047efdf5a5deeee7f8db6ab2
VenGotAGlock <135011195+Onyeka-Okeke@users.noreply.github.com>
2025-05-08 01:35:38 +0100

    Update code.js

--- code.js
+++ code.js
@@ -1,11 +1,78 @@
-function Book(title, author, pages, read){
+const library = [];
+
+function Book(title, author, pages, readStatus, id){
+  if (!new.target) {
+    throw new Error("Book must be called with 'new'");
+  }
   this.title = title;
   this.author = author;
   this.pages = pages;
-  this.read = read;
-  this.info = function() {
-    return `${this.title} by ${this.author}, ${pages} pages, ${this.read}`;
-  };
+  this.readStatus = readStatus;
+  this.id = crypto.randomUUID();
 }
-let firstBook = new Book('The Hobbit', 'J.R.R Tolkien', 295, 'read');
- console.log(firstBook.info());
+function addToLibrary(title, author, pages, readStatus) {
+  const theBook = new Book(title, author, pages, readStatus);
+  library.push(theBook);
+}
+
+addToLibrary("Don Quixote", "Miguel de Cervantes", 550, "read");
+addToLibrary("Wuthering Heights", "Emily Brontë", 728, "not read");
+addToLibrary("The Lord of the Rings", "by J.R.R. Tolkien", 1000, "not read");
+
+   const container = document.getElementById("books_container");
+   
+function displayBooks() {
+  for (const books of library){
+    const bookLists = document.createElement('p');
+    const toggleReadStatus = document.createElement('span');
+    toggleReadStatus.textContent = 'change read status ';
+    toggleReadStatus.addEventListener("click", () => {
+      if (books.readStatus === 'read') {
+        books.readStatus = 'not read';
+      } else if (books.readStatus === 'not read'){
+        books.readStatus = 'read';
+      }
+      container.innerHTML = '';
+      displayBooks();
+    });
+    bookLists.innerHTML = `Title: ${books.title} <br>
+    by<br>
+    Author: ${books.author}<br> 
+    Pages: ${books.pages} pages<br><br>
+    ${books.readStatus}<br><br>`;
+    container.appendChild(bookLists);
+    bookLists.appendChild(toggleReadStatus);
+  }
+}
+displayBooks();
+const modalBtn = document.querySelector("[data-open-modal]");
+const dialogModal = document.querySelector("[data-modal]");
+modalBtn.addEventListener("click", ()=>{
+  dialogModal.showModal();
+});
+dialogModal.addEventListener("click", e => {
+  const dialogDimensions = dialogModal.getBoundingClientRect();
+  if (
+    e.clientX < dialogDimensions.left ||
+    e.clientX > dialogDimensions.right ||
+    e.clientY < dialogDimensions.top ||
+    e.clientY > dialogDimensions.bottom
+  ) {
+    dialogModal.close();
+  }
+});
+const newBookForm = document.querySelector('form');
+newBookForm.addEventListener("submit", function(event) {
+  event.preventDefault();
+  const author_input = document.getElementById('author');
+    const title_input = document.getElementById('title');
+    const pages_input = document.getElementById('pages');
+    const read_status_check = document.getElementById('read-status');
+    const readStatusChecked = read_status_check.checked ? "read" : "not read";
+    addToLibrary(author_input.value, title_input.value, pages_input.value,
+    readStatusChecked);
+    container.innerHTML = '';
+      displayBooks();
+      dialogModal.close();
+      newBookForm.reset();
+});
